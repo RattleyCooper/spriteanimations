149735fa648bf9a65295dd78f9ff4d4811803b4a
py-am-i <6770988+RattleyCooper@users.noreply.github.com>
2024-04-05 21:53:59 -0700

    Update spriteanimations.nim

--- src/spriteanimations.nim
+++ src/spriteanimations.nim
@@ -16,13 +16,17 @@ type
     frames*: int
 
   SpriteAnimation* = object
+    name*: string
     pos*: IVec2
     current*: AnimatedSprite
     animations*: Table[string, AnimatedSprite]
     clock*: FrameCounter
 
   AnimationRenderer* = object
-    spriteAnimations*: seq[SpriteAnimation]
+    sprite*: Table[string, SpriteAnimation]
+
+proc `[]`(renderer: var AnimationRenderer, name: string): var SpriteAnimation =
+  renderer.sprite[name]
 
 proc cmpSprite(a, b: SpriteAnimation): int =
   cmp(a.pos.y + a.current.height, b.pos.y + b.current.height)
@@ -41,9 +45,9 @@ proc play*(sprite: var AnimatedSprite, x: int, y: int) =
   setSpritesheet(sprite.index)
   spr(sprite.frame, x, y, 1, 1, sprite.hflip, sprite.vflip)
 
-proc newSpriteAnimation*(clock: var FrameCounter, pos: IVec2, sprites: varargs[AnimatedSprite]): SpriteAnimation =
+proc newSpriteAnimation*(clock: var FrameCounter, name: string, pos: IVec2, sprites: varargs[AnimatedSprite]): SpriteAnimation =
   result = SpriteAnimation(
-    clock: clock, pos: pos
+    name: name, clock: clock, pos: pos
   )
   for sprite in sprites:
     result.animations[sprite.name] = sprite
@@ -51,7 +55,7 @@ proc newSpriteAnimation*(clock: var FrameCounter, pos: IVec2, sprites: varargs[A
   if result.animations.len > 0:
     result.current = sprites[0]
 
-proc update*(renderer: var SpriteAnimation) =
+proc update*(renderer: var SpriteAnimation, shouldUpdate: bool = true) =
   renderer.current.frame += 1
   if renderer.current.frame - renderer.current.start > renderer.current.frames-1:
     renderer.current.frame = renderer.current.start
@@ -66,22 +70,25 @@ proc play*(sprite: var SpriteAnimation, name: string) =
 proc tick*(sprite: var SpriteAnimation) =
   sprite.clock.tick()
 
-proc ysort*(r: var AnimationRenderer) =
-  r.spriteAnimations.sort(cmpSprite, Ascending)
+proc ysort*(r: var AnimationRenderer): seq[SpriteAnimation] =
+  for k, v in r.sprite.pairs():
+    result.add v
+  result.sort(cmpSprite, Ascending)
 
-proc process*(renderer: var AnimationRenderer, delta: float32) =
-  for i, sprite in renderer.spriteAnimations:
-    renderer.spriteAnimations[i].current.play(sprite.pos.x, sprite.pos.y)
-    renderer.spriteAnimations[i].clock.ControlFlow(delta)
-    renderer.spriteAnimations[i].clock.tick()
+proc process*(renderer: var AnimationRenderer, delta: float32, pauseAnimations: bool = true) =
+  for i, sprite in renderer.sprite.pairs():
+    renderer.sprite[i].current.play(sprite.pos.x, sprite.pos.y)
+    if pauseAnimations: continue
+    renderer.sprite[i].clock.ControlFlow(delta)
+    renderer.sprite[i].clock.tick()
 
 template finalize*(renderer: var AnimationRenderer) =
-  for i, anim in renderer.spriteAnimations:
-    renderer.spriteAnimations[i].clock.run every(1) do():
-      renderer.spriteAnimations[i].update()
+  for i, anim in renderer.sprite:
+    renderer.sprite[i].clock.run every(1) do():
+      renderer.sprite[i].update()
 
 proc newAnimationRenderer*(animations: varargs[SpriteAnimation]): AnimationRenderer =
   result = AnimationRenderer()
   for anim in animations:
-    result.spriteAnimations.add anim
+    result.sprite[anim.name] = anim
 
