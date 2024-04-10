177b47a08fb70d4ce2daf738bd23a1607992d37f
py-am-i <6770988+RattleyCooper@users.noreply.github.com>
2024-04-10 13:43:14 -0700

    Added z-indexing and y-sorting

--- src/spriteanimations.nim
+++ src/spriteanimations.nim
@@ -15,13 +15,14 @@ type
     frame*: int
     frames*: int
     oneShot: bool
+    zindex*: int
 
   SpriteAnimation* = object
     name*: string
     pos*: IVec2
     current*: AnimatedSprite
     animations*: Table[string, AnimatedSprite]
-    clock*: FrameCounter
+    clock*: FrameCounter[SpriteAnimation]
 
   AnimationRenderer* = object
     sprite*: Table[string, SpriteAnimation]
@@ -32,7 +33,7 @@ proc `[]`(renderer: var AnimationRenderer, name: string): var SpriteAnimation =
 proc cmpSprite(a, b: SpriteAnimation): int =
   cmp(a.pos.y + a.current.height, b.pos.y + b.current.height)
 
-proc newAnimatedSprite*(name: string, index: int, start: int, w, h: int, frames: int, oneShot: bool = false): AnimatedSprite =
+proc newAnimatedSprite*(name: string, index: int, start: int, w, h: int, frames: int, oneShot: bool = false, zindex: int=0): AnimatedSprite =
   AnimatedSprite(
     name: name,
     index: index, start: start, 
@@ -40,7 +41,8 @@ proc newAnimatedSprite*(name: string, index: int, start: int, w, h: int, frames:
     hflip: false, vflip: false,
     frame: start,
     frames: frames,
-    oneShot: oneShot
+    oneShot: oneShot,
+    zindex: zindex
   )
 
 proc reset*(a: var AnimatedSprite) =
@@ -51,10 +53,9 @@ proc play*(sprite: var AnimatedSprite, x: int, y: int) =
   # Lock last frame of oneShots
   if sprite.oneShot and sprite.frame - sprite.start == sprite.frames-1:
     sprite.frame = sprite.start + sprite.frames - 1
-
   spr(sprite.frame, x, y, 1, 1, sprite.hflip, sprite.vflip)
 
-proc newSpriteAnimation*(clock: var FrameCounter, name: string, pos: IVec2, sprites: varargs[AnimatedSprite]): SpriteAnimation =
+proc newSpriteAnimation*(clock: var FrameCounter[SpriteAnimation], name: string, pos: IVec2, sprites: varargs[AnimatedSprite]): SpriteAnimation =
   result = SpriteAnimation(
     name: name, clock: clock, pos: pos
   )
@@ -78,27 +79,49 @@ proc play*(sprite: var SpriteAnimation) =
   sprite.current.play(sprite.pos.x, sprite.pos.y)
 
 proc play*(sprite: var SpriteAnimation, name: string) =
+  let hflip = sprite.current.hflip
   sprite.current = sprite.animations[name]
+  sprite.current.hflip = hflip
   sprite.current.play(sprite.pos.x, sprite.pos.y)
 
 proc tick*(sprite: var SpriteAnimation, dt: float32) =
   sprite.clock.tick(dt)
 
 proc ysort*(r: var AnimationRenderer): seq[SpriteAnimation] =
+  var zsorted: Table[int, seq[SpriteAnimation]]
+  var lowz: int
+  var highz: int
   for k, v in r.sprite.pairs():
-    result.add v
-  result.sort(cmpSprite, Ascending)
+    lowz = min(v.current.zindex, lowz)
+    highz = max(v.current.zindex, highz)
+    if zsorted.contains(v.current.zindex):
+      zsorted[v.current.zindex].add v
+    else:
+      zsorted[v.current.zindex] = @[v]
+  for i in lowz..highz:
+    if zsorted.contains(i):
+      var l = zsorted[i]
+      l.sort(cmpSprite, Ascending)
+      result.add l
 
 proc process*(renderer: var AnimationRenderer, delta: float32, pauseAnimations: bool = true) =
-  for i, sprite in renderer.sprite.pairs():
-    renderer.sprite[i].current.play(sprite.pos.x, sprite.pos.y)
+  var sprites = renderer.ysort()
+  for sprite in sprites:
+    renderer.sprite[sprite.name].current.play(sprite.pos.x, sprite.pos.y)
     if pauseAnimations: continue
-    renderer.sprite[i].clock.tick(delta)
+    renderer.sprite[sprite.name].clock.tick(delta)
+
+
+proc add*(renderer: var AnimationRenderer, sprite: var SpriteAnimation) =
+  renderer.sprite[sprite.name] = sprite
+  renderer.sprite[sprite.name].clock.run sprite.every(1) do(sp: var SpriteAnimation):
+    renderer.sprite[sprite.name].update()
 
 template finalize*(renderer: var AnimationRenderer) =
-  for i, anim in renderer.sprite:
-    renderer.sprite[i].clock.run every(1) do():
+  for i, anim in renderer.sprite.pairs():
+    renderer.sprite[i].clock.run renderer.sprite[i].every(1) do(sp: var SpriteAnimation):
       renderer.sprite[i].update()
+      
 
 proc newAnimationRenderer*(animations: varargs[SpriteAnimation]): AnimationRenderer =
   result = AnimationRenderer()
